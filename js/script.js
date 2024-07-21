b71c96b60f50aaeca829d2e551976fb6e040bf0f
assabiri <167641149+assabiri@users.noreply.github.com>
2024-07-21 13:58:55 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Add custom JavaScript here
+function userrScroll() {
+  const navbar = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add("bg-dark");
+    } else {
+      navbar.classList.remove("bg-dark");
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", userrScroll);
